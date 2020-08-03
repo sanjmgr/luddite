56755bb0111da5da0a207f0843248f7def6cce13
sanjaymagarzph <sanjay.magar@zakipoint.com>
2020-08-03 18:34:51 +0545

    Updates

---
+++ precache-manifest.f28c40ad266f142127599c9f4815b9f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3911afd4c97ea25cd382c6a031ccc2ca",
+    "url": "/luddite/index.html"
+  },
+  {
+    "revision": "43ac57b483ac0dd43570",
+    "url": "/luddite/static/css/main.2eb0afc3.chunk.css"
+  },
+  {
+    "revision": "69a53ee7f39d5c3aaf3d",
+    "url": "/luddite/static/js/2.c6c22ec4.chunk.js"
+  },
+  {
+    "revision": "c8ffd5abc06d09fa1ebb018a30d7c2c6",
+    "url": "/luddite/static/js/2.c6c22ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ac57b483ac0dd43570",
+    "url": "/luddite/static/js/main.1ee2bd0d.chunk.js"
+  },
+  {
+    "revision": "dc66ccaad3151c438d62",
+    "url": "/luddite/static/js/runtime-main.383a9598.js"
+  },
+  {
+    "revision": "e164d896782b3e1c22792a99d94e26f2",
+    "url": "/luddite/static/media/MERN.e164d896.png"
+  },
+  {
+    "revision": "76c6d0b76c23ad24ee89f4c6c1a7813d",
+    "url": "/luddite/static/media/Netflix.76c6d0b7.png"
+  },
+  {
+    "revision": "3f04794e1929fc0176eaee1e47d426dd",
+    "url": "/luddite/static/media/Profile.3f04794e.jpg"
+  },
+  {
+    "revision": "36700bee9a1831f2691165d291d62d78",
+    "url": "/luddite/static/media/javascript.36700bee.png"
+  }
+]);
